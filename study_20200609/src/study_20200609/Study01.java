bcaa1243284870c7af8203590a0ac11d5fd2aecb
gooddevuser <gooddevuser@naver.com>
2020-06-14 23:00:35 +0900

    study_20200609

---
+++ study_20200609/src/study_20200609/Study01.java
@@ -0,0 +1,114 @@
+package study_20200609;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Study01 {
+	/**
+	 * =============[ Type ]=============
+	 * @파일명 : Study01.java
+	 * @작성일자 : 2020. 6. 9.
+	 * @작성자 : SEED
+	 * @프로젝트 이름 : study_20200609
+	 * @프로그램 설명 : 
+	 * @변경이력 : 
+	 * =============[ SeedIT ]=============
+	*/
+	// alt+shift+j
+	
+	public class AWDWDWD {
+		private static final String HI = "hello";
+		private static final String HI_MSG = HI;
+		private int age;
+		private String name;
+		public int getAge() {
+			return age;
+		}
+		public void setAge(int age) {
+			this.age = age;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public AWDWDWD(int age, String name) {
+			super();
+			this.age = age;
+			this.name = name;
+		}
+		
+		@Override
+		public String toString() {
+			return "AWDWDWD [age=" + age + ", name=" + name + ", getAge()=" + getAge() + ", getName()=" + getName()
+					+ ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()=" + super.toString()
+					+ "]";
+			
+			
+		}
+		
+		private void sysout() {
+			// TODO Auto-generated method stub
+			hello("", 0);
+		}
+		private int hello(String par, int par2) {
+			String hi = "hi";
+			System.out.println(hi);
+			System.out.println(HI);
+			return par2;
+		}
+		
+		
+	}
+
+	public void gBb(int a) {
+		
+		List<AWDWDWD> AWDWDWD = new ArrayList<>();
+		
+// ctrl+ shift + o
+		try {
+			System.out.println("hOUR");
+			// ctrl + d
+		} catch (Exception e) {
+			// TODO jsy 20200609 : 인증기 구현
+			// FIXME jsy 20200609 : 인증기 값 오류
+			e.printStackTrace();
+		}
+		System.out.println("hourHOUR");
+		System.out.println("hourHOUR");
+		System.out.println("hourHOUR");
+		System.out.println("hourHOUR");
+		System.out.println("hourHOUR");
+		System.out.println("hourHOUR");
+		//ctrl+alt+a // x y (대소문자)
+		System.out.println();
+		for (int limit = 0; limit < 12; limit++) {
+			System.out.println();
+			boolean C = true;
+			if (C) {
+				System.out.println();
+			}
+			if (C) {
+				System.out.println();
+			}
+			System.out.println();
+			System.out.println();
+		}
+		abc();
+		ChangeRenameisCtrlR();
+		System.out.println("asdasds22222asdsd");
+		System.out.println();
+		System.out.println();
+	}
+
+	private void abc() {
+		// TODO Auto-generated method stub
+
+	}
+
+	private void ChangeRenameisCtrlR() {
+		// TODO Auto-generated method stub
+
+	}
+}
